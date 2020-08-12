7afec7f5a1b1f81bd0e3e09971986e1692fbb337
Weijiang Li <weijiangli@WeijiangdeMacBook-Pro.local>
2020-08-12 10:25:13 -0500

    added card components as class info

---
+++ virtual_campus/src/components/ClassInfo.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import CardColumns from 'react-bootstrap/CardColumns';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class ClassInfo extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <CardColumns>
+            <Card bg='light' border='dark' style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>CS</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">CS 173</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+<Card bg='light' border='dark' style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>CS</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">CS 173</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+<Card bg='light' border='dark' style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>CS</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">CS 173</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+<Card bg='light' border='dark' style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>CS</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">CS 173</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+<Card bg='light' border='dark' style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>CS</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">CS 173</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+<Card bg='light' border='dark' style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>CS</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">CS 173</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+<Card bg='light' border='dark' style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>CS</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">CS 173</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+          </CardColumns>
+        );
+    }
+}
+export default ClassInfo;
+
