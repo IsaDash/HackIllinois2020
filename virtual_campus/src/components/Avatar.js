1c8b9a972503a33660043bd9c3fda77ea9cd441f
cguo <cbguo2@illinois.edu>
2020-08-11 20:07:51 -0400

    switched arrow-keys for avatar

--- virtual_campus/src/components/Avatar.js
+++ virtual_campus/src/components/Avatar.js
@@ -32,19 +32,19 @@ class Avatar extends React.Component {
         console.log(e.which || e.keyCode);
         var key_code=e.which||e.keyCode;
 		switch(key_code){
-			case 37: 
+			case 90: 
                 this.moveLeft();
                 console.log("left")
 				break;
-			case 38: 
+			case 83: 
                 this.moveUp();
                 console.log("up")
 				break;
-			case 39: 
+			case 67: 
                 this.moveRight();
                 console.log("right")
 				break;
-			case 40:
+			case 88:
                 this.moveDown();
                 console.log("down")
 				break;						
