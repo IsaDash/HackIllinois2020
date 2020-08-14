2ea49131170a5d5861ba4af46b3989442a586e31
Weijiang Li <weijiangli@WeijiangdeMacBook-Pro.local>
2020-08-13 23:47:23 -0500

    Add Progress bar

---
+++ virtual_campus/src/components/ProgressBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+class Progressbar extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <ProgressBar>
+            <ProgressBar striped variant="success" now={35} key={1} />
+            <ProgressBar variant="warning" now={20} key={2} />
+            <ProgressBar striped variant="danger" now={10} key={3} />
+            </ProgressBar>
+            
+        );
+    }
+}
+export default ProgressBar;
